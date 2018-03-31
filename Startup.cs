d51ed5a55cb8e4dccaa9f79db08281bf64614388
IAmMonmoy <iammonmoy@gmail.com>
2018-03-31 23:11:59 +0600

    added sql server as database to the project

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Cart_Api.Data;
 
 namespace Shopping_Cart
 {
@@ -23,6 +26,9 @@ namespace Shopping_Cart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
             services.AddMvc();
         }
 
