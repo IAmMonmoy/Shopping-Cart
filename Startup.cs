5914dfc6abd15fed2ca7c4c15edf380772049974
IAmMonmoy <iammonmoy@gmail.com>
2018-04-05 01:02:46 +0600

    added token validation

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Shopping_Cart_Api.Data;
 using Shopping_Cart_Api.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Shopping_Cart
 {
@@ -36,9 +39,35 @@ namespace Shopping_Cart
                     .AddEntityFrameworkStores<ApplicationDbContext>()
                     .AddDefaultTokenProviders();
 
-            //JWT Validation
+            //token validation parameters
+           var tokenValidationParameters = new TokenValidationParameters 
+           {
+               ValidateIssuer = true,
+               ValidIssuer = Configuration["JwtIssuerOptions:Issuer"],
+
+               ValidateAudience = true,
+               ValidAudience = Configuration["JwtIssuerOptions:Audience"],
+
+               ValidateIssuerSigningKey = true,
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["JwtIssuerOptions:Key"])),
+
+               RequireExpirationTime = false,
+               ValidateLifetime = true,
+               ClockSkew = TimeSpan.Zero
+           };
             
-            
+            //add jwt bearer token autentication
+           services.AddAuthentication(options => 
+           {
+               options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+               options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+           }).AddJwtBearer( option => 
+           {
+               option.ClaimsIssuer = Configuration["JwtIssuerOptions:Issuer"];
+               option.TokenValidationParameters = tokenValidationParameters;
+               option.SaveToken = true;
+           });
+
             services.AddMvc();
         }
 
