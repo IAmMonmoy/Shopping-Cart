987ed37d127b6b3bc1f5697803e3d56d321f78e0
IAmMonmoy <iammonmoy@gmail.com>
2018-04-18 08:25:55 +0600

    created default admin

---
+++ Models/Constants.cs
@@ -0,0 +1,8 @@
+namespace Shopping_Cart_Api.Models
+{
+    public static class Constants
+    {
+        public const string AdministratorRole = "Administrator";
+        public const string SimpleUser = "ApplicationUser";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -77,16 +77,57 @@ namespace Shopping_Cart
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+                                RoleManager<IdentityRole> roleManager,
+                                UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //create roles needed for application
+
+            EnsureRolesAsync(roleManager).Wait();
+
+            //Create an account and make it administrator
+            AssignAdminRole(userManager).Wait();
+
             app.UseAuthentication();
 
             app.UseMvc();
+        }
+
+        public static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            var alreadyExistsAdmin = await roleManager.RoleExistsAsync(Constants.AdministratorRole);
+            var alreadyExistsSimpleUser = await roleManager.RoleExistsAsync(Constants.SimpleUser);
+
+            if(alreadyExistsAdmin) return; 
+            else await roleManager.CreateAsync(new IdentityRole(Constants.AdministratorRole));
+
+            if(alreadyExistsSimpleUser) return;
+            else await roleManager.CreateAsync(new IdentityRole(Constants.SimpleUser));
+        }
+
+        public static async Task AssignAdminRole(UserManager<ApplicationUser> userManager)
+        {
+            var testAdmin = await userManager.Users.Where(x => x.UserName == "IAmMonmoy").SingleOrDefaultAsync();
+            if(testAdmin == null)
+            {
+                testAdmin = new ApplicationUser
+                {
+                    UserName = "IAmMonmoy",
+                    Email = "iammonmoy@gmail.com"
+                };
+
+                await userManager.CreateAsync(testAdmin,"512345Rrm_");
+            }
+            else
+            {
+               var isAdmin = await userManager.IsInRoleAsync(testAdmin,Constants.AdministratorRole);
+               if(!isAdmin) await userManager.AddToRoleAsync(testAdmin,Constants.AdministratorRole);
+            }
         }
     }
 }
